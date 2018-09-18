cf1fcfe33f9810cb08b281f608adb98c95c87e1f
David Jungst <djungst@gmail.com>
2018-09-18 12:30:53 -0600

    nextId reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import todos from './todos'
+import nextId from './nextId'
 
 const rootReducer = combineReducers({
   todos,
+  nextId,
 })
 
 export default rootReducer
---
+++ src/reducers/nextId.js
@@ -0,0 +1,10 @@
+const nextId = (state = 0, action) => {
+  switch(action.type) {
+    case 'INC_ID':
+      return state + 1
+    default:
+      return state
+  }
+}
+
+export default nextId
